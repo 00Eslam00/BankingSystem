d808f603b21658ba6aa615bccc2dd032f696951a
00Eslam00 <11410120210064@stud.cu.edu.eg>
2022-11-05 16:47:43 +0200

    Added BankAccount Class

---
+++ BankAccount.cpp
@@ -0,0 +1,55 @@
+#include "BankAccount.h"
+
+//Initialize counter
+int BankAccount::counter = 1;
+
+//Parametrized Constructor
+BankAccount::BankAccount(double new_Value) {
+    this->balance = new_Value;
+}
+
+//Default Constructor
+BankAccount::BankAccount() : balance(0) {}
+
+//ID getter
+string BankAccount::get_account_ID() {
+    return (account_ID + to_string(counter));
+}
+
+//Balance getter
+double BankAccount::get_balance() {
+    return balance;
+}
+
+//Balance setter
+void BankAccount::set_balance(double new_value) {
+    this->balance = new_value;
+}
+
+//Withdraw Function
+double BankAccount::withdraw() {
+    double amount;
+    cout << "Please Enter The Amount to Withdraw =========> ";
+    cin >> amount;
+    while (balance - amount < 0) {
+        cout << "Sorry. This is more than what you can withdraw.\n";
+        cout << "Please Enter The Amount to Withdraw =========> ";
+        cin >> amount;
+    }
+    balance -= amount;
+    cout << "Thank you.\n";
+    cout << "Account ID: " << get_account_ID() << endl;
+    cout << "New Balance: " << get_balance() << endl;
+
+}
+
+//Deposit Function
+double BankAccount::deposit() {
+    double amount;
+    cout << "Please Enter The Amount to Deposit =========> ";
+    cin >> amount;
+    balance += amount;
+    cout << "Thank you.\n";
+    cout << "Account ID: " << get_account_ID() << endl;
+    cout << "New Balance: " << get_balance() << endl;
+}
---
+++ BankAccount.h
@@ -0,0 +1,33 @@
+#ifndef BANKACCOUNT_H
+#define BANKACCOUNT_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class BankAccount {
+protected:
+    static int counter;
+    string account_ID = "FCAI - ";
+    double balance;
+public:
+    BankAccount(double new_Value);
+
+    BankAccount();
+
+    string get_account_ID();
+
+    double get_balance();
+
+    void set_balance(double new_value);
+
+    double withdraw();
+
+    double deposit();
+
+};
+
+
+#endif
